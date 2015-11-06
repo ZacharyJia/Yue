98e0eb1185a11b966fb679a99dfd0843469b5c71
Zachary Jia <jia199474@gmail.com>
2015-11-06 18:33:20 +0800

    完成加入活动,退出活动,解散活动功能

--- app/src/main/java/bjtu/cit/yue/DetailActivity.java
+++ app/src/main/java/bjtu/cit/yue/DetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,9 +24,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import bjtu.cit.yue.Utils.PreferenceUtils;
+
 public class DetailActivity extends AppCompatActivity {
 
     private List<User> userList = new ArrayList<User>();
+
+    private Button btn_enroll;
+    private TextView tv_num;
+
+    private int num = 0;
+    private HashMap<String, String> event;
 
 
     @Override
@@ -36,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
         toolbar.setTitle("活动详情");
         setSupportActionBar(toolbar);
 
-
+/*
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +54,10 @@ public class DetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+*/
 
-        HashMap<String, String> event = (HashMap<String, String>) getIntent().getSerializableExtra("event");
+
+        event = (HashMap<String, String>) getIntent().getSerializableExtra("event");
         if(event == null) {
             Toast.makeText(DetailActivity.this, "当前活动不存在！", Toast.LENGTH_LONG).show();
         }
@@ -54,9 +65,35 @@ public class DetailActivity extends AppCompatActivity {
             //显示活动详情
             TextView tv_title = (TextView)findViewById(R.id.title);
             TextView tv_content = (TextView)findViewById(R.id.content);
-            final TextView tv_num = (TextView)findViewById(R.id.num);
+            tv_num = (TextView)findViewById(R.id.num);
             TextView tv_username = (TextView)findViewById(R.id.username);
             TextView tv_deadline = (TextView)findViewById(R.id.deadline);
+
+            updateUserList();
+
+
+            btn_enroll = (Button) findViewById(R.id.enroll);
+            if (event.get("userId").equals(PreferenceUtils.getString(DetailActivity.this, "id", ""))) {
+                btn_enroll.setText("解散该活动");
+            }
+            btn_enroll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (btn_enroll.getText().toString()) {
+                        case "加入":
+                            enroll(PreferenceUtils.getString(DetailActivity.this, "id", ""), event.get("id"));
+                            break;
+                        case "解散该活动":
+                            dismiss(event.get("id"));
+                            break;
+                        case "退出该活动":
+                            unenroll(PreferenceUtils.getString(DetailActivity.this, "id", ""), event.get("id"));
+                            break;
+                        default:
+                            return;
+                    }
+                }
+            });
 
             tv_title.setText(event.get("title"));
             tv_content.setText(event.get("content"));
@@ -64,42 +101,146 @@ public class DetailActivity extends AppCompatActivity {
             tv_num.setText("已参与人数:  " + userList.size() + "人");
             tv_deadline.setText("报名截止日期: " + event.get("deadline"));
 
-            AsyncHttpClient client = new AsyncHttpClient();
-            RequestParams params = new RequestParams();
-            params.put("id", event.get("id"));
-            client.post(DetailActivity.this, "http://yueapi.sinaapp.com/api/getEventUser.php", params, new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(int i, Header[] headers, byte[] bytes) {
-                    Log.e("test", new String(bytes));
-                    try {
-                        JSONObject obj = new JSONObject(new String(bytes));
-                        JSONArray array = obj.getJSONArray("users");
-                        int size = array.length();
-                        for (int j = 0; j < size; j++) {
-                            JSONObject user = array.getJSONObject(j);
-                            User u = new User();
-                            u.setId(Integer.parseInt(user.getString("id")));
-                            u.setUsername(user.getString("username"));
-                            u.setGender(user.getString("gender"));
-                            u.setPhone(user.getString("phone"));
-                            u.setPic(user.getString("pic"));
-                            userList.add(u);
-                        }
-                        tv_num.setText("已参与人数:  " + userList.size() + "人");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-
-                @Override
-                public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-                    Toast.makeText(DetailActivity.this, "网络连接失败，请检查网络后重试！", Toast.LENGTH_LONG).show();
-                }
-            });
-
-
         }
+
+    }
+
+    private void updateUserList() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("id", event.get("id"));
+        client.post(DetailActivity.this, "http://yueapi.sinaapp.com/api/getEventUser.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                Log.e("test", new String(bytes));
+                userList.clear();
+                try {
+                    JSONObject obj = new JSONObject(new String(bytes));
+                    JSONArray array = obj.getJSONArray("users");
+                    int size = array.length();
+                    for (int j = 0; j < size; j++) {
+                        JSONObject user = array.getJSONObject(j);
+                        User u = new User();
+                        u.setId(Integer.parseInt(user.getString("id")));
+                        u.setUsername(user.getString("username"));
+                        u.setGender(user.getString("gender"));
+                        u.setPhone(user.getString("phone"));
+                        u.setPic(user.getString("pic"));
+                        if (user.getString("id").equals(PreferenceUtils.getString(DetailActivity.this, "id", ""))) {
+                            btn_enroll.setText("退出该活动");
+                        }
+                        userList.add(u);
+                    }
+                    num = userList.size();
+                    tv_num.setText("已参与人数:  " + num + "人");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    num = userList.size();
+                    tv_num.setText("已参与人数:  " + num + "人");
+                }
+
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(DetailActivity.this, "网络连接失败，请检查网络后重试！", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void enroll(String userId, String eventId) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("userId", userId + "");
+        params.add("eventId", eventId + "");
+        client.post(DetailActivity.this, "http://yueapi.sinaapp.com/api/enroll.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                String result = new String(bytes);
+                Log.e("test", result);
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    if (obj.getInt("code") == 0) {
+                        btn_enroll.setText("退出该活动");
+                        Toast.makeText(DetailActivity.this, "加入成功!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(DetailActivity.this, "加入失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                updateUserList();
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(DetailActivity.this, "网络连接失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void dismiss(String eventId) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("id", eventId);
+        client.post(DetailActivity.this, "http://yueapi.sinaapp.com/api/dismiss.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                String result = new String(bytes);
+                Log.e("test", result);
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    if (obj.getInt("code") == 0) {
+                        Toast.makeText(DetailActivity.this, "解散成功!", Toast.LENGTH_SHORT).show();
+                        DetailActivity.this.finish();
+                    }
+                    else {
+                        Toast.makeText(DetailActivity.this, "解散失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(DetailActivity.this, "网络连接失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void unenroll(String userId, String eventId) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("userId", userId + "");
+        params.add("eventId", eventId + "");
+        client.post(DetailActivity.this, "http://yueapi.sinaapp.com/api/unenroll.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                String result = new String(bytes);
+                Log.e("test", result);
+                try {
+                    JSONObject obj = new JSONObject(result);
+                    if (obj.getInt("code") == 0) {
+                        btn_enroll.setText("加入");
+                        Toast.makeText(DetailActivity.this, "退出成功!", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(DetailActivity.this, "退出失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                updateUserList();
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(DetailActivity.this, "网络连接失败,请稍后再试!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
